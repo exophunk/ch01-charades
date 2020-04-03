a835b17d74c079f83178e4f13d5a75972fee7d79
Robert Krieg <hello@robertkrieg.ch>
2020-04-03 15:10:13 +0200

    Fix round start selection

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -44,27 +44,30 @@ class GameController extends Controller
 
         $room = Room::findOrFail($request->input('room_id'));
 
+     */
+    public function actionStartRound(Request $request)
+    {
+        $room = Room::findOrFail($request->input('room_id'));
 
-        // SELECT * FROM team_users
+        // SELECT team_users.team_id, team_users.user_id, round_start, MAX(round_start) as last_played FROM team_users
         // LEFT JOIN rounds
-        // ON team_users.user_id = rounds.user_id AND rounds.room_id = 3
-        // WHERE team_users.team_id IN (SELECT id from teams WHERE room_id = 3)
+        // ON team_users.user_id = rounds.user_id AND rounds.room_id = 1
+        // WHERE team_users.team_id IN (SELECT id from teams WHERE room_id = 1)
         // AND
         // (team_users.team_id !=
         //     (SELECT team_id
         //     FROM rounds
-        //     WHERE room_id = 3
+        //     WHERE room_id = 1
         //     ORDER BY round_start DESC
         //     LIMIT 1)
-        // OR (SELECT count(*) FROM rounds r WHERE r.room_id = 3 ) = 0)
-        // ORDER BY round_start, team_users.team_id, team_users.id
-        // LIMIT 1;
+        // OR (SELECT count(*) FROM rounds r WHERE r.room_id = 1 ) = 0)
+        // GROUP BY team_users.team_id, team_users.user_id
+        // ORDER BY last_played, team_users.team_id, team_users.id
+		// LIMIT 1;
 
-
-
-        $result = DB::table('team_users')->select('team_users.team_id', 'team_users.user_id')
+        $result = DB::table('team_users')->select('team_users.team_id', 'team_users.user_id', DB::raw('MAX(round_start) as last_played'))
             ->leftJoin('rounds', function ($join) use ($room) {
-                $join->on('team_users.id', '=', 'rounds.user_id')
+                $join->on('team_users.user_id', '=', 'rounds.user_id')
                      ->where('rounds.room_id', '=', $room->id);
             })
             ->whereRaw('team_users.team_id IN (SELECT id from teams WHERE room_id = ?)', [$room->id])
@@ -78,25 +81,25 @@ class GameController extends Controller
                             ->where('r.room_id', $room->id);
                     }, 0);
             })
-            ->orderBy('round_start')
+            ->groupBy('team_users.team_id')
+            ->groupBy('team_users.user_id')
+            ->orderBy('last_played')
             ->orderBy('team_users.team_id')
             ->orderBy('team_users.id')
             ->first();
-            // ->toSql();
-            // echo($result);
-            // die;
 
-
-
-        if ($result) {
-            $round = new Round();
-            $round->room_id = $room->id;
-            $round->team_id = $result->team_id;
-            $round->user_id = $result->user_id;
-            $round->round_start = Carbon::now();
-            $round->round_end = Carbon::now()->addSeconds($room->round_duration);
-            $round->save();
+        // TODO
+        if (!$result) {
+            return abort(500);
         }
 
+        $round = new Round();
+        $round->room_id = $room->id;
+        $round->team_id = $result->team_id;
+        $round->user_id = $result->user_id;
+        $round->round_start = Carbon::now();
+        $round->round_end = Carbon::now()->addSeconds($room->round_duration);
+        $round->save();
+        event(new StartRound($round));
     }
 }
