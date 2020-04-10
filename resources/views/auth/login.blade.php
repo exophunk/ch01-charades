c020d050e37f023fd2392711e9ddf307d0396f97
Robert Krieg <hello@robertkrieg.ch>
2020-04-10 22:28:25 +0200

    Fix auth forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,11 +18,23 @@
             <div class="field field-text field-email @error('email') field--is-invalid @enderror">
                 <input id="email" placeholder="" type="email" name="email" class="field__input" value="{{ old('email') }}" autocomplete="email" autofocus>
                 <label for="email" class="field__label">{{ __('E-Mail') }}</label>
+
+                @error('email')
+                    <div class="field__error" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="field field-text field-password @error('password') field--is-invalid @enderror">
                 <input id="password" placeholder="" type="password" name="password" class="field__input" autocomplete="current-password" autofocus>
                 <label for="password" class="field__label">{{ __('Passwort') }}</label>
+
+                @error('password')
+                    <div class="field__error" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="field field-checkbox">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,21 +16,45 @@
             <div class="field field-text field-name @error('name') field--is-invalid @enderror">
                 <input id="name" placeholder="" type="name" name="name" class="field__input" value="{{ old('name') }}" autocomplete="name" autofocus>
                 <label for="name" class="field__label">{{ __('Name') }}</label>
+
+                @error('name')
+                    <div class="field__error" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="field field-text field-email @error('email') field--is-invalid @enderror">
                 <input id="email" placeholder="" type="email" name="email" class="field__input" value="{{ old('email') }}" autocomplete="email">
                 <label for="email" class="field__label">{{ __('E-Mail') }}</label>
+
+                @error('email')
+                    <div class="field__error" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="field field-text field-password @error('password') field--is-invalid @enderror">
                 <input id="password" placeholder="" type="password" name="password" class="field__input" autocomplete="new-password">
                 <label for="password" class="field__label">{{ __('Passwort') }}</label>
+
+                @error('password')
+                    <div class="field__error" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
-            <div class="field field-text field-password_confirmation @error('password') field--is-invalid @enderror">
+            <div class="field field-text field-password_confirmation @error('password_confirmation') field--is-invalid @enderror">
                 <input id="password_confirmation" placeholder="" type="password" name="password_confirmation" class="field__input" autocomplete="new-password">
                 <label for="password_confirmation" class="field__label">{{ __('Passwort Bestätigen') }}</label>
+
+                @error('password_confirmation')
+                    <div class="field__error" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
 
